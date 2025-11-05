1dcd23b7dabe1bd2a5031f47ab2ff09d8707024d
jU4Nii <juanibonora2004@gmail.com>
2025-11-05 15:42:41 -0300

    Subida Solución

---
+++ Parcial3/Back/Data1/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Back.Models;
+
+namespace Back.Data1
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=PruebaEF;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+
+
+
+    }
+}
---
+++ Parcial3/Front/Program.cs
@@ -0,0 +1,67 @@
+﻿using Back;
+
+namespace Front
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+
+        public static int PedirEntero(string mensaje)
+        {
+
+            int num;
+            Console.WriteLine(mensaje);
+            while (int.TryParse(Console.ReadLine(), out num) == false)
+            {
+
+                Console.WriteLine("Valor incorrecto, ingreselo nuevamente:");
+
+            }
+
+            return num;
+
+        }
+
+        public static DateTime PedirFecha(string mensaje)
+        {
+            DateTime fecha;
+            Console.WriteLine(mensaje);
+            string input = Console.ReadLine();
+
+            while (DateTime.TryParseExact(input, "dd/MM/yyyy", null,
+                    System.Globalization.DateTimeStyles.None, out fecha) == false)
+            {
+                Console.WriteLine("Fecha inválida, ingrese nuevamente (dd/MM/yyyy):");
+                input = Console.ReadLine();
+            }
+
+            return fecha;
+        }
+
+        //Se usa: DateTime fechaViaje = PedirFecha("Ingrese la fecha del viaje (dd/MM/yyyy):");
+        //Console.WriteLine("Fecha ingresada: " + fechaViaje.ToShortDateString());
+
+        public static string IngresarString(string mensaje)
+        {
+
+            Console.WriteLine(mensaje);
+            string palabra = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(palabra) == true)
+            {
+
+                Console.WriteLine("Valor incorrecto, ingreselo nuevamente:");
+                palabra = Console.ReadLine();
+
+            }
+
+            return palabra;
+
+        }
+
+    }
+}
